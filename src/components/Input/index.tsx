e969755295747b532230c9537acd62aacd6e0b79
Beatriz Lopes <bialopess0911@gmail.com>
2023-01-23 11:51:58 -0300

    Fix input component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,12 +1,29 @@
-import { Input as InputChakra } from '@chakra-ui/react'
+import {
+  Input as InputChakra,
+  InputGroup,
+  InputLeftElement,
+  InputProps as InputPropsChakra,
+} from '@chakra-ui/react';
 
-interface InputProps {
-  placeholder: string;
-  type: string;
+interface InputProps extends InputPropsChakra {
+  icon?: React.ReactNode;
 }
 
-export const Input = ({ placeholder, type }: InputProps) => {
+export const Input = ({ icon, ...rest }: InputProps) => {
   return (
-    <InputChakra size='lg' variant='filled' bg='#262529' placeholder={placeholder} type={type} _hover={{}} />
-  )
-}
+    <InputGroup borderRadius="10px" size="lg">
+      {icon && (
+        <InputLeftElement pointerEvents="none" color="#948F99">
+          {icon}
+        </InputLeftElement>
+      )}
+      <InputChakra
+        variant="filled"
+        bg="#262529"
+        _hover={{}}
+        color="#948F99"
+        {...rest}
+      />
+    </InputGroup>
+  );
+};
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,40 +1,66 @@
-import { Button, Center, Flex, Heading, Image, Input, InputLeftElement, Text } from "@chakra-ui/react";
-import { Card, Container } from "./styles";
+import { Button, Center, Flex, Heading, Image, Text } from '@chakra-ui/react';
+import { Card, Container } from './styles';
 import { HiOutlineEnvelope } from 'react-icons/hi2';
 import { BiLockAlt } from 'react-icons/bi';
-import { useRouter } from "next/router";
-import Link from "next/link";
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import { Input } from '@/components/Input';
 
 export default function Login() {
-    const { push } = useRouter();
+  const { push } = useRouter();
 
-    const onSubmit = () => {
-        push('/Home');
-    }
+  const onSubmit = () => {
+    push('/Home');
+  };
 
-    return (
-            <Container>
-            <Center w='50vw' h='100vh'>
-                <Flex flexDir='column' gap='2rem' w='350px' >
+  return (
+    <Container>
+      <Center w="50vw" h="100vh">
+        <Flex flexDir="column" gap="2rem" w="350px">
+          <div>
+            <Heading
+              color="#FF859B"
+              fontWeight="700"
+              fontStyle="normal"
+              fontSize="49px"
+              lineHeight="63px"
+            >
+              RocketMovies
+            </Heading>
+            <Text color="#CAC4CF">
+              Aplicação para acompanhar tudo que assistir.
+            </Text>
+          </div>
+          <Text color="#CAC4CF" fontWeight="500" textAlign="left">
+            Faça seu login.
+          </Text>
 
-                <div>
-                <Heading color='#FF859B' fontWeight='700' fontStyle='normal' fontSize='49px' lineHeight='63px' >RocketMovies</Heading>
-                <Text color='#CAC4CF'>Aplicação para acompanhar tudo que assistir.</Text>
-                </div>
-                <Text color='#CAC4CF' fontWeight='500' textAlign='left'>Faça seu login.</Text>
+          <Flex flexDir="column" gap=".5rem" as="form" onSubmit={onSubmit}>
+            <Input
+              placeholder="E-mail"
+              type="email"
+              icon={<HiOutlineEnvelope />}
+            />
+            <Input placeholder="Senha" type="password" icon={<BiLockAlt />} />
 
-                <Flex flexDir='column' gap='.5rem' as='form' onSubmit={onSubmit}>
-                   
-                   
-                    <Button size='lg' bg='#FF859B' color='#312E38' type='submit'>Entrar</Button>
-                </Flex>
+            <Button size="lg" bg="#FF859B" color="#312E38" type="submit">
+              Entrar
+            </Button>
+          </Flex>
 
-                <Link href='../SignUp'>
-                <Text textAlign='center' fontWeight='400' color='#FF859B' size='2xl' >Criar conta</Text>
-                </Link>
-                </Flex>
-            </Center>
-            <Image alt='' src='/images/something-magical.png' w='50%' h='100%' />
-   	        </Container>
-    );
-};
+          <Link href="../SignUp">
+            <Text
+              textAlign="center"
+              fontWeight="400"
+              color="#FF859B"
+              size="2xl"
+            >
+              Criar conta
+            </Text>
+          </Link>
+        </Flex>
+      </Center>
+      <Image alt="" src="/images/something-magical.png" w="50%" h="100%" />
+    </Container>
+  );
+}
